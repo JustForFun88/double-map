b2d10ffda53d6d054d37a37fa45b6db11f2486e1
JustForFun88 <100504524+JustForFun88@users.noreply.github.com>
2022-04-04 12:32:09 +0500

    Fix broken links

--- src/map.rs
+++ src/map.rs
@@ -322,18 +322,16 @@ impl<K1, K2, V, S> DHashMap<K1, K2, V, S> {
     ///
     /// # Note
     ///
-    /// Internally [`DHashMap`] use two [`HashMap`]. One of type
+    /// Internally [`DHashMap`] use two [`HashMap`](`std::collections::HashMap`). One of type
     /// `HashMap<K1, (K2, V)>` to hold the `(K2, V)` tuple, and second one of type
     /// `HashMap<K2, K1>` just for holding the primary key of type `K1`.
     ///
-    /// Created iterator iterate only through first [`HashMap`] of type `HashMap<K1, (K2, V)>`.
-    /// So that, if you previously used ['insert_unchecked'] method,
+    /// Created iterator iterate only through first [`HashMap`](`std::collections::HashMap`)
+    /// of type `HashMap<K1, (K2, V)>`.
+    /// So that, if you previously used [`insert_unchecked`](DHashMap::insert_unchecked) method,
     /// this method can return false second keys (key #2) in case of **unsynchronization**
-    /// between first keys of type `K1` and second keys of type `K2`. See ['insert_unchecked']
-    /// method documentation for more.
-    ///
-    /// [`HashMap`]: (`std::collections::HashMap`)
-    /// ['insert_unchecked']: (DHashMap::insert_unchecked)
+    /// between first keys of type `K1` and second keys of type `K2`. See
+    /// [`insert_unchecked`](DHashMap::insert_unchecked) method documentation for more.
     ///
     /// # Examples
     ///
